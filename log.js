e4437b9ba286656836cd06690ffaaa540639454c
Alan <alan@remote.com>
2021-12-12 11:20:22 -0600

    retry

--- log.js
+++ log.js
@@ -1,41 +1,35 @@
-
-import fs from 'fs'
-
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const fs_1 = __importDefault(require("fs"));
 // fatal 
-const BG_RED = '\x1b[41m'
-const BLACK = '\x1b[30m'
-
+const BG_RED = '\x1b[41m';
+const BLACK = '\x1b[30m';
 // error
-const RED = '\x1b[31m'
-
+const RED = '\x1b[31m';
 // warning
-const YELLOW = '\x1b[33m'
-
+const YELLOW = '\x1b[33m';
 // info
-const BLUE = '\x1b[34m'
-
+const BLUE = '\x1b[34m';
 // debug
-const GREEN = '\x1b[32m'
-
+const GREEN = '\x1b[32m';
 // other
-const WHITE = '\x1b[37m'
-const BG_BLACK = '\x1b[40m'
-
+const WHITE = '\x1b[37m';
+const BG_BLACK = '\x1b[40m';
 // DO NOT EXPORT
 const LOG = (arg, color, type) => {
-    const PREFIX = type + ' ' + new Date().toLocaleString()
-    fs.appendFile('debug.log', PREFIX + arg + '\n', (err) => {
-        if (err) throw err
-        console.log(BG_BLACK + color + PREFIX + WHITE + ':' + color, arg, BG_BLACK + WHITE)
-    })
-}
-
-const debug = (arg) => LOG(arg, GREEN, 'DEBUG')
-const info = (arg) => LOG(arg, BLUE, 'INFO')
-const warn = (arg) => LOG(arg, YELLOW, 'WARNING')
-const err = (arg) => LOG(arg, RED, 'ERROR')
-const fatal = (arg) => LOG(arg, BG_RED + BLACK, 'FATAL')
-
-
-export default { debug, info, warn, err, fatal }
-
+    const PREFIX = type + ' ' + new Date().toLocaleString();
+    fs_1.default.appendFile('debug.log', PREFIX + ": " + arg + '\n', (err) => {
+        if (err)
+            throw err;
+        console.log(BG_BLACK + color + PREFIX + WHITE + ':' + color, arg, BG_BLACK + WHITE);
+    });
+};
+const debug = (arg) => LOG(arg, GREEN, 'DEBUG');
+const info = (arg) => LOG(arg, BLUE, 'INFO');
+const warn = (arg) => LOG(arg, YELLOW, 'WARNING');
+const err = (arg) => LOG(arg, RED, 'ERROR');
+const fatal = (arg) => LOG(arg, BG_RED + BLACK, 'FATAL');
+exports.default = { debug, info, warn, err, fatal };
--- log.ts
+++ log.ts
@@ -2,8 +2,8 @@
 import fs from 'fs'
 
 // fatal 
-const BG_RED='\x1b[41m'
-const BLACK= '\x1b[30m'
+const BG_RED = '\x1b[41m'
+const BLACK = '\x1b[30m'
 
 // error
 const RED = '\x1b[31m'
@@ -19,16 +19,14 @@ const GREEN = '\x1b[32m'
 
 // other
 const WHITE = '\x1b[37m'
-const BG_BLACK='\x1b[40m'
+const BG_BLACK = '\x1b[40m'
 
 // DO NOT EXPORT
-const LOG = (arg: any, color: String, type: String) => 
-{
-    const PREFIX=type+' '+new Date().toLocaleString()
-    fs.appendFile('debug.log', PREFIX+arg+'\n', (err)=>
-    {
+const LOG = (arg: any, color: String, type: String) => {
+    const PREFIX = type + ' ' + new Date().toLocaleString()
+    fs.appendFile('debug.log', PREFIX +": "+ arg + '\n', (err) => {
         if (err) throw err
-        console.log(BG_BLACK+color+PREFIX+WHITE+':'+color, arg, BG_BLACK+WHITE)
+        console.log(BG_BLACK + color + PREFIX + WHITE + ':' + color, arg, BG_BLACK + WHITE)
     })
 }
 
@@ -36,8 +34,8 @@ const debug = (arg: any) => LOG(arg, GREEN, 'DEBUG')
 const info = (arg: any) => LOG(arg, BLUE, 'INFO')
 const warn = (arg: any) => LOG(arg, YELLOW, 'WARNING')
 const err = (arg: any) => LOG(arg, RED, 'ERROR')
-const fatal = (arg: any) => LOG(arg, BG_RED+BLACK, 'FATAL')
+const fatal = (arg: any) => LOG(arg, BG_RED + BLACK, 'FATAL')
 
 
-export default {debug, info, warn, err, fatal}
+export default { debug, info, warn, err, fatal }
 
